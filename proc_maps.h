5a25bf3a8a9d9eff6881a26df4f019d620b1c605
zhuang233 <1552798761@qq.com>
2023-10-22 09:45:07 +0800

    使用3.18.71的get_proc_maps_list，好像能用

--- proc_maps.h
+++ proc_maps.h
@@ -547,6 +547,208 @@ MY_STATIC int get_proc_maps_list(struct pid* proc_pid_struct, size_t max_path_le
 #endif
 
 
+
+#if MY_LINUX_VERSION_CODE == KERNEL_VERSION(3,18,20)
+/* Check if the vma is being used as a stack by this task */
+MY_STATIC int vm_is_stack_for_task(struct task_struct *t,
+	struct vm_area_struct *vma) {
+	return (vma->vm_start <= KSTK_ESP(t) && vma->vm_end >= KSTK_ESP(t));
+}
+
+
+/*
+ * Check if the vma is being used as a stack.
+ * If is_group is non-zero, check in the entire thread group or else
+ * just check in the current task. Returns the task_struct of the task
+ * that the vma is stack for. Must be called under rcu_read_lock().
+ */
+struct task_struct *task_of_stack(struct task_struct *task,
+	struct vm_area_struct *vma, bool in_group) {
+	if (vm_is_stack_for_task(task, vma))
+		return task;
+
+	if (in_group) {
+		struct task_struct *t;
+
+		for_each_thread(task, t) {
+			if (vm_is_stack_for_task(t, vma))
+				return t;
+		}
+	}
+
+	return NULL;
+}
+
+
+MY_STATIC pid_t pid_of_stack(struct task_struct *task,
+	struct vm_area_struct *vma, bool is_pid) {
+	pid_t ret = 0;
+
+	rcu_read_lock();
+	task = task_of_stack(task, vma, is_pid);
+	if (task) {
+		ret = task->pid;
+	}
+	rcu_read_unlock();
+
+	return ret;
+}
+
+
+MY_STATIC int get_proc_maps_list(struct pid* proc_pid_struct, size_t max_path_length, char* lpBuf, size_t buf_size, bool is_kernel_buf, int *have_pass) {
+	struct task_struct *task;
+	struct mm_struct *mm;
+	struct vm_area_struct *vma;
+	char new_path[MY_PATH_MAX_LEN];
+	char path_buf[MY_PATH_MAX_LEN];
+	int success = 0;
+	size_t copy_pos;
+	size_t end_pos;
+
+	if (max_path_length <= 0) {
+		return -1;
+	}
+
+	task = pid_task(proc_pid_struct, PIDTYPE_PID);
+	if (!task) {
+		return -2;
+	}
+
+	mm = get_task_mm(task);
+	if (!mm) {
+		return -3;
+	}
+
+	if (is_kernel_buf) {
+		memset(lpBuf, 0, buf_size);
+	}
+	//else if (clear_user(lpBuf, buf_size)) { return -4; } //清空用户的缓冲区
+
+
+	copy_pos = (size_t)lpBuf;
+	end_pos = (size_t)((size_t)lpBuf + buf_size);
+
+	if (down_read_mmap_lock(mm) != 0) {
+		mmput(mm);
+		return -4;
+	}
+	for (vma = mm->mmap; vma; vma = vma->vm_next) {
+		unsigned long start, end;
+		unsigned char flags[4];
+		struct file * vm_file;
+		if (copy_pos >= end_pos) {
+			if (have_pass) {
+				*have_pass = 1;
+			}
+			break;
+		}
+		start = vma->vm_start;
+		end = vma->vm_end;
+
+
+		flags[0] = vma->vm_flags & VM_READ ? '\x01' : '\x00';
+		flags[1] = vma->vm_flags & VM_WRITE ? '\x01' : '\x00';
+		flags[2] = vma->vm_flags & VM_EXEC ? '\x01' : '\x00';
+		flags[3] = vma->vm_flags & VM_MAYSHARE ? '\x01' : '\x00';
+
+
+		memset(new_path, 0, sizeof(new_path));
+		vm_file = get_vm_file(vma);
+		if (vm_file) {
+			char *path;
+			memset(path_buf, 0, sizeof(path_buf));
+			path = d_path(&vm_file->f_path, path_buf, sizeof(path_buf));
+			if (path > 0) {
+				strncat(new_path, path, sizeof(new_path) - 1);
+			}
+		} else if (vma->vm_mm && vma->vm_start == (long)vma->vm_mm->context.vdso) {
+			if ((sizeof(new_path) - strlen(new_path) - 7) >= 0) {
+				strcat(new_path, "[vdso]");
+			}
+		} else {
+			if (vma->vm_start <= mm->brk &&
+				vma->vm_end >= mm->start_brk) {
+				if ((sizeof(new_path) - strlen(new_path) - 7) >= 0) {
+					strcat(new_path, "[heap]");
+				}
+			} else {
+				pid_t tid = pid_of_stack(task, vma, 1);
+				if (tid != 0) {
+					/*
+					 * Thread stack in /proc/PID/task/TID/maps or
+					 * the main process stack.
+					 */
+
+					 /* Thread stack in /proc/PID/maps */
+					if (vma->vm_start <= mm->start_stack &&
+						vma->vm_end >= mm->start_stack) {
+						if ((sizeof(new_path) - strlen(new_path) - 8) >= 0) {
+							strcat(new_path, "[stack]");
+						}
+					} else {
+						snprintf(new_path, sizeof(new_path), "[stack:%d]", tid);
+					}
+				}
+
+			}
+
+		}
+		if (is_kernel_buf) {
+			memcpy((void*)copy_pos, &start, 8);
+			copy_pos += 8;
+			memcpy((void*)copy_pos, &end, 8);
+			copy_pos += 8;
+			memcpy((void*)copy_pos, &flags, 4);
+			copy_pos += 4;
+			memcpy((void*)copy_pos, &new_path, max_path_length > MY_PATH_MAX_LEN ? MY_PATH_MAX_LEN : max_path_length - 1);
+			copy_pos += max_path_length;
+		} else {
+			//内核空间->用户空间交换数据
+			if (!!x_copy_to_user((void*)copy_pos, &start, 8)) {
+				if (have_pass) {
+					*have_pass = 1;
+				}
+				break;
+			}
+			copy_pos += 8;
+
+			if (!!x_copy_to_user((void*)copy_pos, &end, 8)) {
+				if (have_pass) {
+					*have_pass = 1;
+				}
+				break;
+			}
+			copy_pos += 8;
+
+			if (!!x_copy_to_user((void*)copy_pos, &flags, 4)) {
+				if (have_pass) {
+					*have_pass = 1;
+				}
+				break;
+			}
+			copy_pos += 4;
+
+			if (!!x_copy_to_user((void*)copy_pos, &new_path, max_path_length > MY_PATH_MAX_LEN ? MY_PATH_MAX_LEN : max_path_length - 1)) {
+				if (have_pass) {
+					*have_pass = 1;
+				}
+				break;
+			}
+			copy_pos += max_path_length;
+
+		}
+		success++;
+	}
+	up_read_mmap_lock(mm);
+	mmput(mm);
+
+	return success;
+}
+
+#endif
+
+
+
 #if MY_LINUX_VERSION_CODE == KERNEL_VERSION(3,18,71)
 /* Check if the vma is being used as a stack by this task */
 MY_STATIC int vm_is_stack_for_task(struct task_struct *t,
